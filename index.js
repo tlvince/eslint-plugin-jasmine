0a20648a81c22eb882ecc62a82325e1e9d382b09
Remco Haszing <remcohaszing@gmail.com>
2015-09-09 20:16:12 +0200

    feat(missing-expect): add missing-expect rule
    This rule checks for it block which do not contain any expectations.

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ module.exports = {
     'no-focused-tests': require('./lib/rules/no-focused-tests'),
     'no-disabled-tests': require('./lib/rules/no-disabled-tests'),
     'no-suite-dupes': require('./lib/rules/no-suite-dupes'),
-    'no-spec-dupes': require('./lib/rules/no-spec-dupes')
+    'no-spec-dupes': require('./lib/rules/no-spec-dupes'),
+    'missing-expect': require('./lib/rules/missing-expect')
   },
   rulesConfig: {
     'no-focused-tests': 2,
     'no-disabled-tests': 1,
     'no-suite-dupes': 1,
-    'no-spec-dupes': 1
+    'no-spec-dupes': 1,
+    'missing-expect': 0
   }
 };
