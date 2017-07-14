3a15a5a78aa2c77f9cedda687c9253ef3045e717
Diana Suvorova <dsuvorova@DSuvorova-196.local>
2017-07-14 15:00:05 -0700

    fix: cleanup fixes

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ module.exports = {
     'no-expect-in-setup-teardown': require('./lib/rules/no-expect-in-setup-teardown'),
     'new-line-between-declarations': require('./lib/rules/new-line-between-declarations'),
     'new-line-before-expect': require('./lib/rules/new-line-before-expect'),
-    'prefer-jasmine-matcher': require('./lib/rules/prefer-jasmine-matcher')
+    'prefer-jasmine-matcher': require('./lib/rules/prefer-jasmine-matcher'),
+    'prefer-toHaveBeenCalledWith': require('./lib/rules/prefer-toHaveBeenCalledWith')
   },
   configs: {
     recommended: {
@@ -38,7 +39,7 @@ module.exports = {
         'jasmine/new-line-between-declarations': 1,
         'jasmine/new-line-before-expect': 1,
         'jasmine/prefer-jasmine-matcher': 1,
-        'jasmine/prefer-toHaveBeenCalledWith': 0,
+        'jasmine/prefer-toHaveBeenCalledWith': 0
       }
     }
   }
