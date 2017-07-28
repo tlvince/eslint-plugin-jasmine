ec1c7765e3f254915167ef38543a76ae4e55e360
Diana Suvorova <diana.suvorova@gmail.com>
2017-07-28 11:51:56 -0700

    fix: supporting arrow functions

--- test/rules/no-suite-callback-args.js
+++ test/rules/no-suite-callback-args.js
@@ -2,8 +2,13 @@
 
 var rule = require('../../lib/rules/no-suite-callback-args')
 var RuleTester = require('eslint').RuleTester
+var linesToCode = require('../helpers/lines_to_code')
 
-var eslintTester = new RuleTester()
+const parserOptions = {
+  ecmaVersion: 8
+}
+
+var eslintTester = new RuleTester({parserOptions})
 
 eslintTester.run('describe-with-done', rule, {
   valid: [
@@ -13,7 +18,19 @@ eslintTester.run('describe-with-done', rule, {
     'xdescribe("My suite", function() {});',
     'describe("My suite", function() {\nit("A spec", function() {});\n});',
     'describe("My suite", function() {\nit("A spec", function(done) {});\n});',
-    'describe("My suite", function() {\nit("A spec", function(done) {\ndone();\n});\n });'
+    'describe("My suite", function() {\nit("A spec", function(done) {\ndone();\n});\n });',
+    linesToCode([
+      'describe("", () => {',
+      ' it("", () => {',
+      ' });',
+      '});'
+    ]),
+    linesToCode([
+      'describe("", () => {',
+      ' it("", (done) => {',
+      ' });',
+      '});'
+    ])
   ],
 
   invalid: [
@@ -72,6 +89,31 @@ eslintTester.run('describe-with-done', rule, {
           message: "Unexpected argument in suite's callback"
         }
       ]
-    }
-  ]
+    },
+    {
+      code: linesToCode([
+        'describe("", (done) => {',
+        ' it("", (done) => {',
+        ' });',
+        '});'
+      ]),
+      errors: [
+        {
+          message: "Unexpected argument in suite's callback"
+        }
+      ]
+    },
+    {
+      code: linesToCode([
+        'describe("", () => {',
+        ' describe("", (done) => {',
+        ' });',
+        '});'
+      ]),
+      errors: [
+        {
+          message: "Unexpected argument in suite's callback"
+        }
+      ]
+    }]
 })
