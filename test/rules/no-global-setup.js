0f11ae56d215b6b2b3ea5f2bc151053a437e97c2
Jean-Etienne LaVallee <jelavallee@iflection.com>
2017-07-29 23:20:34 +0000

    fix(no-global-setup): allow global setup in helpers report tweaks

--- test/rules/no-global-setup.js
+++ test/rules/no-global-setup.js
@@ -19,14 +19,15 @@ eslintTester.run('no-global-setup', rule, {
       message: 'Do not use `beforeEach` outside a `describe` except for in global helpers.'
     }]
   }, {
-    code: 'afterEach(function() {}); describe(function() {})',
+    code: 'beforeEach(function() {}); afterEach(function() {}); beforeAll(function() {}); afterAll(function() {}); describe(function() {})',
     errors: [{
+      message: 'Do not use `beforeEach` outside a `describe` except for in global helpers.'
+    }, {
       message: 'Do not use `afterEach` outside a `describe` except for in global helpers.'
-    }]
-  }, {
-    code: 'beforeEach(function() {}); describe(function() {}); afterEach(function() {})',
-    errors: [{
-      message: 'Do not use `beforeEach, afterEach` outside a `describe` except for in global helpers.'
+    }, {
+      message: 'Do not use `beforeAll` outside a `describe` except for in global helpers.'
+    }, {
+      message: 'Do not use `afterAll` outside a `describe` except for in global helpers.'
     }]
   }]
 })
