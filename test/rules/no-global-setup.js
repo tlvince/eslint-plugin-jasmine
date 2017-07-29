debc62041a39ec005e66a5c121006cb6bc5af036
Jean-Etienne LaVallee <jelavallee@iflection.com>
2017-07-29 19:10:29 +0000

    fix(no-global-setup): allow global setup in helpers

--- test/rules/no-global-setup.js
+++ test/rules/no-global-setup.js
@@ -6,17 +6,21 @@ var RuleTester = require('eslint').RuleTester
 var eslintTester = new RuleTester()
 
 eslintTester.run('no-global-setup', rule, {
-  valid: [
-    'describe("", function() { beforeEach(function() {}) })'
-  ],
-  invalid: [
-    {
-      code: 'beforeEach(function() {})',
-      errors: [{message: 'Do not use `beforeEach` outside a `describe`.'}]
-    },
-    {
-      code: 'afterEach(function() {})',
-      errors: [{message: 'Do not use `afterEach` outside a `describe`.'}]
-    }
-  ]
+  valid: [{
+    code: 'describe("", function() { beforeEach(function() {}) })'
+  }, {
+    code: 'beforeEach(function() {})',
+    filename: 'tests/helpers/mocks/mockBeforeEachHelper.js'
+  }],
+  invalid: [{
+    code: 'beforeEach(function() {})',
+    errors: [{
+      message: 'Do not use `beforeEach` outside a `describe`.'
+    }]
+  }, {
+    code: 'afterEach(function() {})',
+    errors: [{
+      message: 'Do not use `afterEach` outside a `describe`.'
+    }]
+  }]
 })
