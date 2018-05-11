ea8c687f439bac62bf00449eb342f301cd0ebb99
rabidkitten <ditus@rocketmail.com>
2018-05-11 11:41:59 -0400

    Update capitalized-it.js
    Coding of initial body of test.

--- test/rules/capitalized-it.js
+++ test/rules/capitalized-it.js
@@ -1,1 +1,47 @@
+'use strict'
+
+var rule = require('../../lib/rules/capitalized-it')
+var RuleTester = require('eslint').RuleTester
+
+var eslintTester = new RuleTester()
+
+eslintTester.run('capitalized-it', rule, {
+  valid: [
+    {
+      options: [
+        'always'
+      ],
+      code: toCode([
+        'it("This is valid.", function() {});'        
+      ])
+    },   
+    {
+      options: [
+        'never'
+      ],
+      code: toCode([
+        'it("this is valid.", function() {});'        
+      ])
+    },  
+  ],
+
+  invalid: [
+    {
+      options: [
+        'always'
+      ],
+      code: toCode([
+        'it("this is invalid.", function() {});'        
+      ])
+    },   
+    {
+      options: [
+        'never'
+      ],
+      code: toCode([
+        'it("This is invalid.", function() {});'        
+      ])
+    },  
+  ]
+})
 
