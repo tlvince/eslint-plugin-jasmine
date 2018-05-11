9542aef2746a888dfb50ae64f3815b593a3d4735
rabidkitten <ditus@rocketmail.com>
2018-05-11 11:55:04 -0400

    Update capitalized-it.js
    Fixed indentation.

--- test/rules/capitalized-it.js
+++ test/rules/capitalized-it.js
@@ -19,39 +19,35 @@ function toCode (lines, description) {
 
 eslintTester.run('capitalized-it', rule, {
   valid: [{
-      options: [
-        'always'
-      ],
-      code: toCode([
-        'it("This is valid.", function() {});'
-      ])
-    },
-    {
-      options: [
-        'never'
-      ],
-      code: toCode([
-        'it("this is valid.", function() {});'
-      ])
-    },
-  ],
-
-  invalid: [
-    {
-      options: [
-        'always'
-      ],
-      code: toCode([
-        'it("this is invalid.", function() {});'
-      ])
-    },
-    {
-      options: [
-        'never'
-      ],
-      code: toCode([
-        'it("This is invalid.", function() {});'
-      ])
-    },
-  ]
+    options: [
+      'always'
+    ],
+    code: toCode([
+      'it("This is valid.", function() {});'
+    ])
+  },
+  {
+    options: [
+      'never'
+    ],
+    code: toCode([
+      'it("this is valid.", function() {});'
+    ])
+  }],
+  invalid: [{
+    options: [
+      'always'
+    ],
+    code: toCode([
+      'it("this is invalid.", function() {});'
+    ])
+  },
+  {
+    options: [
+      'never'
+    ],
+    code: toCode([
+      'it("This is invalid.", function() {});'
+    ])
+  }]
 })
